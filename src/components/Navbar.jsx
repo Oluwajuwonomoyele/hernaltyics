ae22e21d176b67969bdbb63dea48d2bdaa673ce9
Olutomisin Oluwajuwon <jolutomisin@gmail.com>
2023-03-02 18:11:04 +0100

    navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,13 @@
 import logo from '../assets/logo.png';
 import '../styles/Navbar.css'
+import {
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    Flex,
+} from '@chakra-ui/react';
+import { FaChevronDown } from "react-icons/fa";
 
 const Navbar = () => {
     return (  
@@ -12,8 +20,110 @@ const Navbar = () => {
                 <div className="nav-links-container">
                     <div className='nav-links'>
                         <p>about us</p>
-                        <p>our communities</p>
-                        <p>election data</p>
+                        <Menu >
+                            {({isOpen}) => (
+                                <>
+                                    <MenuButton
+                                    px={2}
+                                    py={1}
+                                    outline='none'
+                                    border='none'
+                                    textTransform='uppercase'
+                                    fontWeight='bold'
+                                    background='transparent'
+                                    color='white'
+                                    fontSize='14px'
+                                    cursor='pointer'
+                                    isActive={isOpen}>
+                                    <Flex gap={3} alignItems='center'>
+                                    <p>our communities</p>
+                                    <FaChevronDown />
+                                    </Flex>
+                                    </MenuButton>
+                                    <MenuList 
+                                    minW={230}
+                                    color='rgb(56,97,116)'
+                                    background='white'
+                                    outline='none'
+                                    border='none'
+                                    >
+                                        <MenuItem 
+                                        fontSize='14px'
+                                        p={3}
+                                        outline='none'
+                                        fontWeight='semibold'
+                                        borderColor='rgb(56,97,116)'
+                                        borderWidth={1}>General User</MenuItem>
+                                        <MenuItem
+                                        fontSize='14px'
+                                        p={3}
+                                        outline='none'
+                                        fontWeight='semibold'
+                                        borderColor='rgb(56,97,116)'
+                                        borderWidth={1}>Decide To Run</MenuItem>
+                                        <MenuItem
+                                        fontSize='14px'
+                                        p={3}
+                                        outline='none'
+                                        fontWeight='semibold'
+                                        borderColor='rgb(56,97,116)'
+                                        borderWidth={1}>Election Candidates</MenuItem>
+                                        <MenuItem
+                                        fontSize='14px'
+                                        p={3}
+                                        outline='none'
+                                        fontWeight='semibold'
+                                        borderColor='rgb(56,97,116)'
+                                        borderWidth={1}>Reporters</MenuItem>
+                                    </MenuList>
+                                </>
+                            )}
+                        </Menu>
+                        <Menu >
+                            {({isOpen}) => (
+                                <>
+                                    <MenuButton
+                                    px={2}
+                                    py={1}
+                                    outline='none'
+                                    border='none'
+                                    textTransform='uppercase'
+                                    fontWeight='bold'
+                                    background='transparent'
+                                    color='white'
+                                    fontSize='14px'
+                                    cursor='pointer'
+                                    isActive={isOpen}>
+                                    <Flex gap={3} alignItems='center'>
+                                    <p>election data</p>
+                                    <FaChevronDown />
+                                    </Flex>
+                                    </MenuButton>
+                                    <MenuList 
+                                    minW={230}
+                                    color='rgb(56,97,116)'
+                                    background='white'
+                                    outline='none'
+                                    border='none'
+                                    >
+                                        <MenuItem 
+                                        fontSize='14px'
+                                        p={3}
+                                        outline='none'
+                                        fontWeight='semibold'
+                                        borderColor='rgb(238,238,240)'
+                                        borderWidth={2}>Election Day Live Updates</MenuItem>
+                                        <MenuItem
+                                        fontSize='14px'
+                                        p={3}
+                                        outline='none'
+                                        fontWeight='semibold'
+                                        borderColor='rgb(238,238,240)'
+                                        borderWidth={2}>Post-Election Analysis</MenuItem>
+                                    </MenuList>
+                                </>
+                            )}
+                        </Menu>
                         <p>e-buddy</p>
                         <p>veo platform</p>
                     </div>
--- src/index.js
+++ src/index.js
@@ -2,10 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ChakraBaseProvider, extendBaseTheme } from '@chakra-ui/react';
+import chakraTheme from '@chakra-ui/theme'
+
+const { Menu, Button } = chakraTheme.components
+
+const theme = extendBaseTheme({
+  styles: {
+    global: () => ({
+      body: {
+        bg: '',
+      }
+    }),
+    components: {
+      Menu,
+      Button,
+    }
+  }
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraBaseProvider resetCSS={false} theme={theme}> 
+      <App />
+    </ChakraBaseProvider>
   </React.StrictMode>
 );
